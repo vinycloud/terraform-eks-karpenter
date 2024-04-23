e17a31461edee4d2207b30acdc688e2d1c66e970
Vinicius Fialho <84024631+vinycloud@users.noreply.github.com>
2024-04-23 18:53:31 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "my-cluster-karpenter"
+  cluster_version = "1.29"
+
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = "vpc-086962b1368da360"
+  subnet_ids               = ["subnet-05bcc6b707f8ba3af", "subnet-09d2967a31128cf37"]
+  control_plane_subnet_ids = ["subnet-0735a53a17f965706", "subnet-08bc969fa89b5ec8d"]
+
+module "karpenter" {
+  source = "terraform-aws-modules/eks/aws//modules/karpenter"
+
+  cluster_name = module.eks.cluster_name
+
+  # Attach additional IAM policies to the Karpenter node IAM role
+  node_iam_role_additional_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}  
