4ecee5cdee35ae060a5155e4878960d5781a195c
Vinicius <vinycloud@outlook.com>
2024-04-26 09:12:49 -0300

    Building module...

--- main.tf
+++ main.tf
@@ -1,40 +1,9 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+module "eks-karpenter" {
+  source  = "./terraform"
 
-  cluster_name    = "my-cluster-karpenter"
+  cluster_name    = "eks-karpenter"
   cluster_version = "1.29"
 
-  cluster_endpoint_public_access  = true
+}
 
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
 
-  vpc_id                   = "vpc-086962b1368da360"
-  subnet_ids               = ["subnet-05bcc6b707f8ba3af", "subnet-09d2967a31128cf37"]
-  control_plane_subnet_ids = ["subnet-05bcc6b707f8ba3af", "subnet-09d2967a31128cf37"]
-
-module "karpenter" {
-  source = "terraform-aws-modules/eks/aws//modules/karpenter"
-
-  cluster_name = module.eks.cluster_name
-
-  # Attach additional IAM policies to the Karpenter node IAM role
-  node_iam_role_additional_policies = {
-    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  }
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
-}  
